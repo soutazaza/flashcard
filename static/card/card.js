1a9f279a2ab8f04c5be28f2ea14192e00a6fcd35
sogaw <souta.ogawa@gmail.com>
2024-11-01 19:03:24 +0700

    0.0.2

---
+++ static/card/card.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    var element = document.getElementById('cardId');
+    var cardId = element.innerText || element.textContent;
+});
+
