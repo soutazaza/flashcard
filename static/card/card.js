010086916f553bdf0b15e8d9058772711756c580
sogaw <souta.ogawa@gmail.com>
2024-11-03 19:25:09 +0700

    0.0.3

--- static/card/card.js
+++ static/card/card.js
@@ -1,5 +1,16 @@
+var IsCardAvailable = true
+
 document.addEventListener("DOMContentLoaded", function (event) {
     var element = document.getElementById('cardId');
     var cardId = element.innerText || element.textContent;
+
+    var list = JSON.parse(localStorage.getItem(cardId)) || null
+
+    if(!list) {
+        IsCardAvailable = false
+        this.body.style.backgroundColor = "#BD0000";
+        element.innerText = "ERROR: Card not found 😭😭😭😭😭"
+        return;
+    }
 });
 
