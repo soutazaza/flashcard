0a7507ceac071d8afe03b278236992fa694ff77d
sogaw <souta.ogawa@gmail.com>
2024-11-11 20:19:30 +0700

    0.0.4

--- static/card/card.js
+++ static/card/card.js
@@ -2,15 +2,19 @@ var IsCardAvailable = true
 
 document.addEventListener("DOMContentLoaded", function (event) {
     var element = document.getElementById('cardId');
+    var textbox = document.getElementById('textbox');
+
     var cardId = element.innerText || element.textContent;
 
     var list = JSON.parse(localStorage.getItem(cardId)) || null
 
+    console.log(list)
+
     if(!list) {
         IsCardAvailable = false
         this.body.style.backgroundColor = "#BD0000";
-        element.innerText = "ERROR: Card not found 😭😭😭😭😭"
+        
+        textbox.innerHTML = "Card not found"
         return;
     }
 });
-
